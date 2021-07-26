00978673006364e3ce5889d164a93a3d0278f72f
Andre Lara <alosbh@gmail.com>
2021-07-26 15:00:00 -0300

    readline parser

--- src/main.js
+++ src/main.js
@@ -25,67 +25,6 @@ var topicMqtt = process.env.MQTT_TOPIC
 // var topicMqtt = "rtls/blescan"
 var client  = mqtt.connect({hostname: mqttBroker, port: mqttPort, clientId:'watcherRTLS'})
 
-const options = {
-  hostname: 'rtlsdatabase',
-
-  port: '3014',
-
-  path: "/submit",
-
-  method: "POST",
-
-  headers: {
-    "Content-Type": "application/json"
-
-    //'Content-Length': data.length
-  }
-};
-
-const options2 = {
-    hostname: 'rtlsdatabase',
-
-    port: '3014',
-
-  path: "/mayday",
-
-  method: "POST",
-
-  headers: {
-    "Content-Type": "application/json"
-
-    //'Content-Length': data.length
-  }
-};
-
-
-// var myVar = setInterval(myTimer, 5000);
-
-function myTimer() {
-  
-  var d = new Date();
-  
-  const req = http.request(options2, res => {
-    
-
-      res.on("data", d => {
-        process.stdout.write(d)
-      });
-  });
-
-  req.on("error", error => {
-    console.error("Mayday HTTP Request Error: " + error);
-  });
-  
-  var mtst = Date.now();
-  var jsondata = {};
-  jsondata['hostname'] = MacAddr;
-  jsondata['last_update'] = mtst/1000;
-  req.write(JSON.stringify(jsondata));
-  req.end();
-  
-}
-
-
 scanner.onadvertisement = ad => {
   if (ad.beaconType == "eddystoneTlm") {
     ad.localName = hostname;
@@ -100,7 +39,6 @@ scanner.onadvertisement = ad => {
 var scanmode = 'dongle'
 switch(scanmode) {
   case "onboard":
-
 
 	scanner.startScan().then(() => {
 	    console.log("Scanning for BLE devices...");
@@ -126,13 +64,12 @@ switch(scanmode) {
 	
 	port.on("open", function () {
 	    console.log('Serial port opened');
-	
 		const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
-		
-		parser.on('data', console.log)
+		parser.on('data', (d)=>{
+            console.log("Data:" + d)
+        })
 	
 	});
-
 
     break;
   
